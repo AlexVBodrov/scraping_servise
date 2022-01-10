c3a001ddfec2e8f0e40c45111dd42c4f42f50eb1
AlexVBodrov <alexvbodrov@mail.ru>
2022-01-10 19:34:15 +0300

    add Models,

--- src/scraping_service/urls.py
+++ src/scraping_service/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from scraping.views import home_view
 from .views import home
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home),
-    path('', home),
+    path('', home_view),
 ]
