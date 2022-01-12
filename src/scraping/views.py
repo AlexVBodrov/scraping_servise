d0264d511c76d5e3f4f2e099269b594861524c04
AlexVBodrov <alexvbodrov@mail.ru>
2022-01-13 00:46:00 +0300

    add home_views. add Boostrap classes

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Vacancy
+import datetime
+
 
 # Create your views here.
 def home_view(request):
+    date = datetime.datetime.now().date()
+    name = 'Dave'
     qs = Vacancy.objects.all()
-    _content = {'object_list': qs}
-    return render(request, 'home.html', _content)
+    _content = {'object_list': qs, 'date': date, 'name': name}
+    return render(request, 'scraping/home.html', _content)
 
--- src/scraping_service/urls.py
+++ src/scraping_service/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#from .scraping.views import home_view
 from scraping.views import home_view
-from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
     path('', home_view),
 ]
