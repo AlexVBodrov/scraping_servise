b0da5ff7fdd89e66996be750ef0d1da0d993f92e
AlexVBodrov <alexvbodrov@mail.ru>
2022-01-14 18:17:23 +0300

    add form. add Boostrap classes. add Views_all. add accounts

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 from .models import Vacancy
+from .forms import FindForm
 import datetime
 
 
 # Create your views here.
 def home_view(request):
+    form = FindForm()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__slug'] = city
+        if language:
+            _filter['language__slug'] = language
+        qs = Vacancy.objects.filter(**_filter)
     date = datetime.datetime.now().date()
     name = 'Dave'
-    qs = Vacancy.objects.all()
-    _content = {'object_list': qs, 'date': date, 'name': name}
+    _content = {'object_list': qs, 'date': date, 'name': name, 'form': form}
     return render(request, 'scraping/home.html', _content)
 
+
+def view_all_vacancy(request):
+    qs = Vacancy.objects.all()
+    date = datetime.datetime.now().date()
+    name = 'Dave'
+    _content = {'object_list': qs, 'date': date, 'name': name}
+    return render(request, 'scraping/view_all_vacancy.html', _content)
